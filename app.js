1ff2985605f897ca34fa97a71f1ca232259bcfca
Andrew <kerovap@mail.ru>
2020-10-26 15:15:37 +0700

    2

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 19999999
+      message: 19999
+    },
+    methods: {
+      formatTooltip(val) {
+        return val / 100;
+      }
     }
   })
